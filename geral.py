798dfeb20e1ece10377a1972989c094a302318b7
Carlos Abreu <carlos.abreu@zoho.com>
2024-12-16 19:54:27 +0000

    Reshape column

--- geral.py
+++ geral.py
@@ -3,9 +3,10 @@ import numpy as np # type: ignore
 import pandas as pd # type: ignore
 
 df = pd.DataFrame({'Name': ['Ann', 'Bob'], 'Age': [20, 50]})
-print(df)
+# print(df)
 
 ds = pd.Series([1, 2, 3], index=['a', 'b', 'c'])
 # print(ds)
 
-print(df[['Name', 'Age']])
+# print(df[['Name', 'Age']])
+print(df['Name'])
