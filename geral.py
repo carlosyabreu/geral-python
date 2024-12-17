d85b0131203cd8adfe4f7a95791e74ad5c2ad0d3
Carlos Abreu <carlos.abreu@zoho.com>
2024-12-17 01:57:52 +0000

    Another dataframe

--- geral.py
+++ geral.py
@@ -2,11 +2,14 @@
 import numpy as np # type: ignore
 import pandas as pd # type: ignore
 
-df = pd.DataFrame({'Name': ['Ann', 'Bob'], 'Age': [20, 50]})
+# df = pd.DataFrame({'Name': ['Ann', 'Bob'], 'Age': [20, 50]})
 # print(df)
 
 ds = pd.Series([1, 2, 3], index=['a', 'b', 'c'])
 # print(ds)
 
 # print(df[['Name', 'Age']])
-print(df['Name'])
+# print(df['Name'])
+
+df = pd.DataFrame({'Name': ['Peter', 'Charles'], 'Age': [83, 18]})
+print(df)
